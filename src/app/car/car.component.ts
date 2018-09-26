4f793d6efade8caf6f4a974cdb16dd8a48da8848
Volodymyr <z_valdemar@ukr.net>
2018-09-26 17:22:39 +0300

    make logic to delete and add car

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Car } from '../car.model';
 
 @Component({
   selector: 'app-car',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+  @Input() car: Car;
+  @Output() deleteCar = new EventEmitter<Car>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onBuy() {
+    this.car.isSold = true;
+  }
+
+  onDelete() {
+    this.deleteCar.emit(this.car);
   }
 
 }
